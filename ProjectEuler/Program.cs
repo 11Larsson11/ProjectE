9d811678a84879e70722fdc375c881248fec1fe4
Richard Larsson <richard.auerhahn@gmail.com>
2022-05-23 15:35:05 +0200

    Add project files.

---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+//Project Euler #1
+
+
+var list = new List<int>();
+
+//Checking which numbers between 1 and 1000 that are divisable with 3 or 5
+
+for (int i = 1; i < 1000; i++)
+{
+
+    if ((i % 3 == 0) | (i % 5 == 0) == true)
+    {
+        list.Add(i);
+    }
+
+}
+
+
+Console.WriteLine("Project Euler");
+Console.WriteLine("Problem #1: The sum of all natural numbers below 1000, divisable with 3 & 5 are " + list.Sum());
